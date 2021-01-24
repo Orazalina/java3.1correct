17489717512f0a23f0492ed1ebbe1840a74c1b67
Anara Orazalina <orazalina.anara@inbox.ru>
2021-01-24 21:02:02 +0600

    main added

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 2150;
+        int Amount = 20;
+
+        int BonusMile = TicketPrice / Amount;
+        System.out.println(BonusMile);
+    }
+}
