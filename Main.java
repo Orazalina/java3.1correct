228e7db6203183d97abdf3a93f4c6593832e02a2
Anara Orazalina <orazalina.anara@inbox.ru>
2021-01-25 21:42:41 +0600

    corrected

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketPrice = 2150;
-        int Amount = 20;
+        int ticketPrice = 2150;
+        int amount = 20;
 
-        int BonusMile = TicketPrice / Amount;
-        System.out.println(BonusMile);
+        int bonusMile = ticketPrice / amount;
+
+        System.out.println(bonusMile);
     }
 }
